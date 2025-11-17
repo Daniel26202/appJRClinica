d6b1d342e16717f2a1ee700fcd89c388c6e16ff9
W. Daniel <129122054+Daniel26202@users.noreply.github.com>
2025-11-17 11:58:52 -0400

    configuracion de app JR

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+// https://docs.expo.dev/guides/using-eslint/
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
+const { rules } = require("eslint-config-prettier");
+
+module.exports = defineConfig([
+    expoConfig,
+    {
+        ignores: ["dist/*"],
+    },
+    {
+        exports: ["expo", "prettier"],
+        plugins: ["prettier"],
+        rules: {
+            "prettier/prettier": "error",
+        },
+    },
+]);
